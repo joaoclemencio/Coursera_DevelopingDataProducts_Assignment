ff03e7ec4bb11e155754f9aabf10800fd6e9d70f
Joao Clemencio <joao.clemencio@gmail.com>
2016-07-11 03:33:47 +0100

    cleanup 1

--- server.R
+++ server.R
@@ -12,14 +12,3 @@ shinyServer(
     output$tree1 <- renderPlot(fancyRpartPlot(iris_rpart$finalModel))
   }
 )
-
-
-
-
-# data.frame(
-#   sd_min = t(data.frame(lapply(iris[,-5], mean))) - (4 * t(data.frame(lapply(iris[,-5], sd)))),
-#   min = t(data.frame(lapply(iris[,-5], min))),
-#   mean = t(data.frame(lapply(iris[,-5], mean))),
-#   max = t(data.frame(lapply(iris[,-5], max))),
-#   sd_max = t(data.frame(lapply(iris[,-5], mean))) + (4 * t(data.frame(lapply(iris[,-5], sd))))
-# )
